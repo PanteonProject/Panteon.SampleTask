eb8a144bc83017e0e8dd92d9e7c1f73e7b73404a
Ziya SARIKAYA <sarikayaziya@gmail.com>
2015-08-21 09:23:14 +0300

    code refactoring

--- SampleTask/Exports.cs
+++ SampleTask/Exports.cs
@@ -1,6 +1,9 @@
 using System.ComponentModel.Composition;
 using Autofac;
+using Autofac.Extras.NLog;
+using Panteon.SampleTask.Configuration;
 using Panteon.Sdk;
+using Panteon.Sdk.Utils;
 
 namespace Panteon.SampleTask
 {
@@ -14,7 +17,15 @@ namespace Panteon.SampleTask
                 var builder = new ContainerBuilder();
 
                 builder.RegisterModule<CoreModule>();
-                builder.RegisterModule<TaskModule>();
+
+                builder.RegisterType<EnvironmentWrapper>().As<IEnvironmentWrapper>().SingleInstance();
+
+                builder.RegisterType<JsonNetSerializer>().As<IJsonSerializer>().SingleInstance();
+
+                builder.Register(context => new SampleTaskConfigProvider(context.Resolve<ILogger>()).ParseSettings())
+                    .AsImplementedInterfaces().SingleInstance();
+
+                builder.RegisterType<SampleTask>().As<IPanteonTask>();
 
                 return builder;
             }
