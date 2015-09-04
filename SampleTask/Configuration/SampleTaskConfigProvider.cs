0bfda17a2515ccb41003e6e1778a6d49324b5ae9
Ziya SARIKAYA <sarikayaziya@gmail.com>
2015-09-04 09:55:24 +0300

    refactor configuration

--- SampleTask/Configuration/SampleTaskConfigProvider.cs
+++ SampleTask/Configuration/SampleTaskConfigProvider.cs
@@ -1,6 +1,4 @@
-﻿using System.IO;
-using System.Reflection;
-using Autofac.Extras.NLog;
+﻿using Autofac.Extras.NLog;
 using Panteon.Sdk.Configuration;
 using Panteon.Sdk.IO;
 using Panteon.Sdk.Utils;
@@ -9,32 +7,9 @@ namespace Panteon.SampleTask.Configuration
 {
     public class SampleTaskConfigProvider : TaskConfigProviderBase<SampleTaskSettings>
     {
-        private readonly IJsonSerializer _jsonSerializer;
-        private readonly IFileReader _fileReader;
-
         public SampleTaskConfigProvider(ILogger logger, IJsonSerializer jsonSerializer, IFileReader fileReader)
-            : base(logger)
+            : base(logger, jsonSerializer, fileReader)
         {
-            _jsonSerializer = jsonSerializer;
-            _fileReader = fileReader;
-        }
-
-        public override SampleTaskSettings ParseSettings(string configFilePath = null)
-        {
-            string asmLocation = Assembly.GetExecutingAssembly().Location;
-
-            string directoryName = Path.GetDirectoryName(asmLocation);
-
-            if (!string.IsNullOrEmpty(directoryName))
-            {
-                configFilePath = configFilePath ?? Path.Combine(directoryName, "config.json");
-            }
-
-            FileContentResult result = _fileReader.ReadFileContent(configFilePath);
-
-            SampleTaskSettings settings = _jsonSerializer.Deserialize<SampleTaskSettings>(result.Content);
-
-            return settings;
         }
     }
 }
