efd1a14313e1318e74948a801b639057625d7cec
Ziya SARIKAYA <sarikayaziya@gmail.com>
2015-09-14 14:43:23 +0300

    fix config load

--- SampleTask/Configuration/SampleTaskConfigProvider.cs
+++ SampleTask/Configuration/SampleTaskConfigProvider.cs
@@ -1,4 +1,6 @@
-﻿using Autofac.Extras.NLog;
+﻿using System.IO;
+using System.Reflection;
+using Autofac.Extras.NLog;
 using Panteon.Sdk.Configuration;
 using Panteon.Sdk.IO;
 using Panteon.Sdk.Utils;
@@ -7,9 +9,23 @@ namespace Panteon.SampleTask.Configuration
 {
     public class SampleTaskConfigProvider : TaskConfigProviderBase<SampleTaskSettings>
     {
+        private readonly IJsonSerializer _jsonSerializer;
+        private readonly IFileReader _fileReader;
+
         public SampleTaskConfigProvider(ILogger logger, IJsonSerializer jsonSerializer, IFileReader fileReader)
             : base(logger, jsonSerializer, fileReader)
         {
+            _jsonSerializer = jsonSerializer;
+            _fileReader = fileReader;
+        }
+
+        public override SampleTaskSettings ParseSettings(string configFilePath = null)
+        {
+            string directoryName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (!string.IsNullOrEmpty(directoryName))
+                configFilePath = configFilePath ?? Path.Combine(directoryName, "config.json");
+
+            return _jsonSerializer.Deserialize<SampleTaskSettings>(_fileReader.ReadFileContent(configFilePath).Content);
         }
     }
 }
