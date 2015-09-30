72bc5f8e3a0057e3205941452eebbdeea58f2506
Ziya SARIKAYA <sarikayaziya@gmail.com>
2015-09-30 15:47:31 +0300

    cr

--- SampleTask/Configuration/SampleTaskConfigProvider.cs
+++ SampleTask/Configuration/SampleTaskConfigProvider.cs
@@ -1,4 +1,6 @@
-﻿using Autofac.Extras.NLog;
+﻿using System.IO;
+using System.Reflection;
+using Autofac.Extras.NLog;
 using Panteon.Sdk.Configuration;
 using Panteon.Sdk.IO;
 using Panteon.Sdk.Utils;
@@ -7,9 +9,36 @@ namespace Panteon.SampleTask.Configuration
 {
     public class SampleTaskConfigProvider : TaskConfigProviderBase<SampleTaskSettings>
     {
+        private readonly IJsonSerializer _jsonSerializer;
+        private readonly IFileReader _fileReader;
+
         public SampleTaskConfigProvider(ILogger logger, IJsonSerializer jsonSerializer, IFileReader fileReader)
             : base(logger, jsonSerializer, fileReader)
         {
+            _jsonSerializer = jsonSerializer;
+            _fileReader = fileReader;
+        }
+
+        public override SampleTaskSettings ParseSettings(string configFilePath = null)
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+
+            var originalJobFolder = Path.GetDirectoryName(executingAssembly.CodeBase);
+
+            if (!string.IsNullOrEmpty(originalJobFolder))
+            {
+                string combine = Path.Combine(originalJobFolder, "config.json");
+                if (File.Exists(combine))
+                {
+                    return _jsonSerializer.Deserialize<SampleTaskSettings>(_fileReader.ReadFileContent(combine).Content);
+                }
+            }
+
+            string directoryName = Path.GetDirectoryName(executingAssembly.Location);
+            if (!string.IsNullOrEmpty(directoryName))
+                configFilePath = configFilePath ?? Path.Combine(directoryName, "config.json");
+
+            return _jsonSerializer.Deserialize<SampleTaskSettings>(_fileReader.ReadFileContent(configFilePath).Content);
         }
     }
 }
