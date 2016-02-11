fce06dfe2cabfc59dc297fd17e3a7e4d501d8927
Ziya SARIKAYA <sarikayaziya@gmail.com>
2016-02-11 17:31:43 +0200

    add custom nlog config

--- SampleTask/Exports.cs
+++ SampleTask/Exports.cs
@@ -1,10 +1,14 @@
 using System.ComponentModel.Composition;
+using System.IO;
+using System.Reflection;
 using Autofac;
 using Autofac.Extras.NLog;
+using NLog;
 using Panteon.SampleTask.Configuration;
 using Panteon.Sdk;
 using Panteon.Sdk.IO;
 using Panteon.Sdk.Utils;
+using ILogger = Autofac.Extras.NLog.ILogger;
 
 namespace Panteon.SampleTask
 {
@@ -19,24 +23,44 @@ namespace Panteon.SampleTask
 
                 builder.RegisterModule<WorkerModule>();
 
+                RegisterCustomNLogger(builder);
+
                 builder.RegisterType<JsonNetSerializer>().As<IJsonSerializer>().SingleInstance();
 
                 builder.RegisterType<FileSystem>().As<IFileSystem>().SingleInstance();
                 builder.RegisterType<FileReader>().As<IFileReader>().SingleInstance();
 
-
-                builder.Register(context => new SampleTaskConfigProvider(
-                    context.Resolve<ILogger>(),
-                    context.Resolve<IJsonSerializer>(),
-                    context.Resolve<IFileReader>()
-                    )
-                    .ParseSettings()
-                    ).AsImplementedInterfaces()
+                builder.Register(
+                    context =>
+                        new SampleTaskConfigProvider(context.Resolve<ILogger>(), context.Resolve<IJsonSerializer>(),
+                            context.Resolve<IFileReader>()).ParseSettings())
+                    .AsImplementedInterfaces()
                     .SingleInstance();
 
                 builder.RegisterType<SampleTask>().As<IPanteonWorker>();
 
                 return builder;
+            }
+        }
+
+        private void RegisterCustomNLogger(ContainerBuilder builder)
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+
+            var originalJobFolder = Path.GetDirectoryName(executingAssembly.CodeBase);
+
+            if (!string.IsNullOrEmpty(originalJobFolder))
+            {
+                string combine = Path.Combine(originalJobFolder.Replace("file:\\", ""), "NLog.config");
+
+                if (File.Exists(combine))
+                {
+                    LogManager.Configuration = new NLog.Config.XmlLoggingConfiguration(combine, true);
+
+                    Logger currentClassLogger = LogManager.GetCurrentClassLogger();
+
+                    builder.Register(c => new LoggerAdapter(currentClassLogger)).As<ILogger>().SingleInstance();
+                }
             }
         }
     }
